8b51425929716ae197d47698bb02784442cf1470
LeonTan <145657C@sit.nyp.edu.sg>
2015-10-29 17:06:18 +0800

    ...

---
+++ src/demo/BookDBAO.java
@@ -0,0 +1,26 @@
+package demo;
+
+/**
+ * Created by Leon Tan on 29/10/2015.
+ */
+public class BookDBAO {
+
+    public BookDetails getBookDetails(String bookId) {
+        // return dummy book for testing now
+        return createDummyBook(bookId);
+    }
+
+    private BookDetails createDummyBook(String bookId) {
+        BookDetails details = new BookDetails();
+        details.setBookId(bookId);
+        details.setDescription("Web components for developers talks about how web components can transfor the way you develop application for the web");
+        details.setFirstName("Java");
+        details.setSurname("Expert");
+        details.setInventory(5);
+        details.setOnSale(false);
+        details.setPrice(5.9f);
+        details.setTitle("Web components for developers");
+        details.setYear(2015);
+        return details;
+    }
+}
